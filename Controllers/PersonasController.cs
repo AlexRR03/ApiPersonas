605d43f367dc5a65c796a851d3046c521317f374
AlexRR03 <alexciempo03@gmail.com>
2025-03-21 10:25:11 +0100

    Añadir Swagger a la api

---
+++ Controllers/PersonasController.cs
@@ -0,0 +1,57 @@
+﻿using ApiPersonas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private List<Persona> personas;
+        public PersonasController() {
+            this.personas = new List<Persona>();
+            Persona p = new Persona
+            {
+                IdPersona = 1,
+                Nombre = "Alejandro",
+                Email = "alex@mail.com",
+                Edad = 21
+            };
+            this.personas.Add(p);
+            Persona p1 = new Persona
+            {
+                IdPersona = 2,
+                Nombre = "Lucia",
+                Email = "lu@mail.com",
+                Edad = 20
+            };
+            this.personas.Add(p1);
+            Persona p2 = new Persona
+            {
+                IdPersona = 3,
+                Nombre = "David",
+                Email = "david@mail.com",
+                Edad = 22
+            };
+            this.personas.Add(p2);
+            Persona p3 = new Persona
+            {
+                IdPersona = 4,
+                Nombre = "Fernando",
+                Email = "fer@mail.com",
+                Edad = 21
+            };
+            this.personas.Add(p3);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Persona>> Get() {
+            return this.personas;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Persona>Get(int id) {
+            return this.personas.FirstOrDefault(x => x.IdPersona == id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
+    app.MapOpenApi();
 
 app.UseHttpsRedirection();
+app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json","ApiPersonas"));
 
 app.UseAuthorization();
 
